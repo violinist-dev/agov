4a4940dd2cc86f99b7a528fab62d1da707c61481
Chris Skene <chris@xtfer.com>
2013-08-21 15:08:44 +1000

    [#16106] Common base SCSS for state themes

---
+++ themes/agov_states/config.rb
@@ -0,0 +1,50 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+# Leave this as expanded for now.
+output_style = :expanded
+
+# Uncomment the line below to output debug info your css files.
+# Be sure to re-comment it before commiting.
+# sass_options = {:debug_info => true}
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
+
+
+# Require any additional compass plugins installed on your system.
+require 'breakpoint'
+
+require 'fileutils'
+
+states = {
+  'nsw' => 'waratah',
+  'act' => 'bluebell'
+}
+
+
+on_stylesheet_saved do |file|
+  states.each do |state, symbol|
+    if File.exists?(file) && File.basename(file) == state + ".css"
+      FileUtils.mv(file, File.dirname(file) + "/../" + symbol + "/css/styles.css")
+    end
+  end
+end
