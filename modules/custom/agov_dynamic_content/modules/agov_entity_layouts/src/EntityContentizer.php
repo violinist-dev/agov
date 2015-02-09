7685d07528c2f58f5a38e3defff08954d5321f93
Chris Skene <chris@xtfer.com>
2015-02-09 15:12:58 +1100

    [#28946] Fix unprovisionable Entity Content with Paragraphs

--- modules/custom/agov_dynamic_content/modules/agov_entity_layouts/src/EntityContentizer.php
+++ modules/custom/agov_dynamic_content/modules/agov_entity_layouts/src/EntityContentizer.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains a
+ * Contains a EntityContentizer
  *
  * @license GPL v2 http://www.fsf.org/licensing/licenses/gpl.html
  * @author Chris Skene chris at previousnext dot com dot au
@@ -13,10 +13,20 @@ namespace Drupal\agov_entity_layouts;
 
 
 /**
- * Class HookEntityView
+ * Class EntityContentizer
  * @package Drupal\agov_entity_layouts
  */
-class HookEntityView {
+class EntityContentizer {
+
+  /**
+   * Defines the layout field name.
+   */
+  const FIELD_LAYOUT = 'field_layout_mode';
+
+  /**
+   * Defines the paragraphs field name.
+   */
+  const FIELD_PARAGRAPHS = 'field_entity_content';
 
   /**
    * The content buffer.
@@ -30,7 +40,7 @@ class HookEntityView {
    *
    * @static
    * @return static
-   *   Returns an instance of HookEntityView
+   *   Returns an instance of EntityContentizer
    */
   static public function init() {
 
@@ -42,26 +52,28 @@ class HookEntityView {
    *
    * @param object $entity
    *   The entity.
-   * @param string $type
+   * @param string $entity_type
    *   The entity type.
    * @param string $view_mode
    *   The view mode.
    * @param string $langcode
    *   The langcode
    */
-  public function entityView($entity, $type, $view_mode, $langcode) {
+  public function entityView($entity, $entity_type, $view_mode, $langcode) {
     $this->contentBuffer = $entity->content;
 
     if ($view_mode == 'full') {
-      if (isset($entity->field_layout_mode[LANGUAGE_NONE][0]['value']) && !empty($entity->field_layout_mode[LANGUAGE_NONE][0]['value'])) {
-        $layout_mode = check_plain($entity->field_layout_mode[LANGUAGE_NONE][0]['value']);
+      $layout_lang = $this->fieldLanguage($entity_type, $entity, $this::FIELD_LAYOUT, $langcode);
+      if (isset($entity->{$this::FIELD_LAYOUT}[$layout_lang][0]['value']) && !empty($entity->{$this::FIELD_LAYOUT}[$layout_lang][0]['value'])) {
 
-        if ($layout_mode == 'paragraphs') {
-          $this->processParagraphsEntity($entity, $type, $langcode);
-
+        // The layout field switches content between normal and paragraphs
+        // (dynamic) mode.
+        if ($entity->{$this::FIELD_LAYOUT}[$layout_lang][0]['value'] == AGOV_ENTITY_LAYOUTS_MODE_DYNAMIC) {
+          $this->processParagraphsEntity($entity, $entity_type, $langcode);
         }
         else {
-          unset($entity->content['field_paragraph_content']);
+          // The paragraphs field should not appear in normal mode.
+          unset($entity->content[self::FIELD_PARAGRAPHS]);
         }
       }
     }
@@ -72,16 +84,16 @@ class HookEntityView {
    *
    * @param object $entity
    *   The entity.
-   * @param string $type
+   * @param string $entity_type
    *   The entity type.
    * @param string $langcode
    *   The langcode
    */
-  protected function processParagraphsEntity($entity, $type, $langcode) {
+  protected function processParagraphsEntity($entity, $entity_type, $langcode) {
 
     // Replace the entity content with the content of the paragraphs
     // field.
-    $entity->content = field_view_field($type, $entity, 'field_paragraph_content', array('label' => 'hidden'), $langcode);
+    $entity->content = field_view_field($entity_type, $entity, self::FIELD_PARAGRAPHS, array('label' => 'hidden'), $langcode);
 
     // If there is an 'entity_content' paragraph item, replace its content
     // with the current entity.
@@ -98,5 +110,24 @@ class HookEntityView {
         }
       }
     }
+  }
+
+  /**
+   * Get the correct language for a field.
+   *
+   * @param string $entity_type
+   *   The entity type.
+   * @param object $entity
+   *   The entity
+   * @param string $field_name
+   *   The field name
+   * @param string $langcode
+   *   The language code
+   *
+   * @return string
+   *   The language code.
+   */
+  protected function fieldLanguage($entity_type, $entity, $field_name, $langcode) {
+    return field_language($entity_type, $entity, $field_name, $langcode);
   }
 }
