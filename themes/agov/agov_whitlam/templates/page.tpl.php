eda955382a076132a10b46b90d420e05e251a94b
Hieren Feb <feb@geekpolis.com>
2015-06-01 09:42:28 +0700

    [#31958] Update page.tpl file, changed postion nav region.

--- themes/agov/agov_whitlam/templates/page.tpl.php
+++ themes/agov/agov_whitlam/templates/page.tpl.php
@@ -48,26 +48,7 @@
   </div>
 </header>
 
-<div class="main">
-
-  <div class="main-content" role="main">
-    <?php print render($page['highlighted']); ?>
-    <?php print $breadcrumb; ?>
-    <a href="#skip-link" class="visually-hidden--focusable" id="main-content">Back to top</a>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h1><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php print $messages; ?>
-    <?php print render($tabs); ?>
-    <?php print render($page['help']); ?>
-    <?php if ($action_links): ?>
-      <ul class="action-links"><?php print render($action_links); ?></ul>
-    <?php endif; ?>
-    <?php print render($page['content']); ?>
-    <?php print $feed_icons; ?>
-  </div>
+<div class="main layout-center">
 
   <div class="main-navigation">
 
@@ -94,6 +75,25 @@
 
     <?php print render($page['navigation']); ?>
 
+  </div>
+
+  <div class="main-content" role="main">
+    <?php print render($page['highlighted']); ?>
+    <?php print $breadcrumb; ?>
+    <a href="#skip-link" class="visually-hidden--focusable" id="main-content">Back to top</a>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+      <h1><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php print $messages; ?>
+    <?php print render($tabs); ?>
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+    <?php print render($page['content']); ?>
+    <?php print $feed_icons; ?>
   </div>
 
   <?php
