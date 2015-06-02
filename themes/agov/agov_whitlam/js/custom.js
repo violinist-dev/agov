e7dc929b83df19f717f9ee56d86944aeb686505b
bien.vu <tienbienit1992@gmail.com>
2015-06-02 17:11:24 +0700

    [#31042] Add navbar reponsive

--- themes/agov/agov_whitlam/js/custom.js
+++ themes/agov/agov_whitlam/js/custom.js
@@ -5,7 +5,6 @@
  * In order for this JavaScript to be loaded on pages, see the instructions in
  * the README.txt next to this file.
  */
-
 // JavaScript should be made compatible with libraries other than jQuery by
 // wrapping it with an "anonymous closure". See:
 // - http://drupal.org/node/1446420
@@ -15,38 +14,24 @@
 $(window).load(function(){
   $(document).ready(function() {
     // Variable for responsive js
-    $(".block-superfish").prepend('<div class="search-icon open active">menu<i class="fa fa fa-bars"></i></div>');
-    $(".block-superfish").prepend('<div class="search-icon close">close<i class="fa fa-close"></i></div>');
-    $(".search-icon.open").click(function(){
+    $(".block-superfish").prepend('<div class="menu-icon open active">menu<i class="fa fa fa-bars"></i></div>');
+    $(".block-superfish").prepend('<div class="menu-icon close">close<i class="fa fa-close"></i></div>');
+    $("a.menuparent").after('<div class="sub-menu-icon"><i class="fa fa-plus"></i></div>');
+    $(".menu-icon.open").click(function(){
       $(this).removeClass("active");
-      $(".search-icon.close").addClass("active");
+      $(".menu-icon.close").addClass("active");
       $(".sf-menu").addClass("active");
     });
 
-    $(".search-icon.close").click(function(){
+    $(".menu-icon.close").click(function(){
       $(this).removeClass("active");
-      $(".search-icon.open").addClass("active");
+      $(".menu-icon.open").addClass("active");
       $(".sf-menu").removeClass("active");
     });
 
-    var $small = 568;
-    var $medium = 768;
-
-    function resize_responsive(){
-      var width = window.innerWidth || document.documentElement.clientWidth;
-
-      if (width < $medium) {
-      }
-
-      // Add equal heihgt for Content , sidebar for devices has width >= 1024px . Ex:
-      if(width > $medium){
-      }
-    }
-
-    //Call function responsive()
-    resize_responsive();
-    $(window).resize(function() {
-      resize_responsive();
+    $(".sub-menu-icon").click(function(){
+      $(this).toggleClass("active");
+      $(this).next().toggleClass("active");
     });
   });
 });
