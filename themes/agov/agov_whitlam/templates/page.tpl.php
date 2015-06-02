74413a82457a9a9201a25bbd6023419d64e1e419
Hieren Feb <feb@geekpolis.com>
2015-06-02 14:17:41 +0700

    [#31040] Add nav background, removed double main-menu, style second-menu position.

--- themes/agov/agov_whitlam/templates/page.tpl.php
+++ themes/agov/agov_whitlam/templates/page.tpl.php
@@ -48,29 +48,8 @@
   </div>
 </header>
 
-<div class="navigation">
+<div class="navigation clearfix">
   <div class="layout-center">
-
-    <?php if ($main_menu): ?>
-      <nav class="main-menu" role="navigation" id="main-menu" tabindex="-1">
-        <?php
-        // This code snippet is hard to modify. We recommend turning off the
-        // "Main menu" on your sub-theme's settings form, deleting this PHP
-        // code block, and, instead, using the "Menu block" module.
-        // @see https://drupal.org/project/menu_block
-        print theme('links__system_main_menu', array(
-          'links' => $main_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Main menu'),
-            'level' => 'h2',
-            'class' => array('visually-hidden'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
 
     <?php print render($page['navigation']); ?>
 
