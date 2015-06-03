35ffc888d7b52fec0d757ca1e4bdbdca791e7dd9
Kim Pepper <kim@pepper.id.au>
2015-06-03 13:50:34 +1000

    [#31035] Fixes codestyle issues

--- themes/agov/agov_whitlam/template.php
+++ themes/agov/agov_whitlam/template.php
@@ -95,20 +95,31 @@ function agov_whitlam_preprocess_comment(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
+
+/**
+ * Implements hook_preprocess_comment().
+ */
 function agov_whitlam_preprocess_comment(&$variables, $hook) {
   $comment = $variables['comment'];
-  $variables['submitted'] = t('<span class="field author">!username</span> - <span class="field date-month">'.format_date($comment->created,'custom','l d M, Y').'</span>', array('!username' => $variables['author']));
+  $variables['submitted'] = t('<span class="field author">!username</span> - <span class="field date-month">!date</span>',
+    array(
+      '!username' => $variables['author'],
+      '!date' => format_date($comment->created, 'custom', 'l d M, Y'),
+    ));
   if (variable_get('comment_subject_field_' . $variables['node']->type, 1) == 0) {
     $variables['title'] = '';
   }
 }
 
+/**
+ * Implements hook_preprocess_comment_wrapper().
+ */
 function agov_whitlam_preprocess_comment_wrapper(&$variables, $hook) {
-  if($variables['node']->comment_count) {
-  $variables['total_comments_in_node']= $variables['node']->comment_count;
+  if ($variables['node']->comment_count) {
+    $variables['total_comments_in_node'] = $variables['node']->comment_count;
   }
   else {
-  $variables['total_comments_in_node'] = 0;
+    $variables['total_comments_in_node'] = 0;
   }
 }
 
--- themes/agov/agov_whitlam/templates/comment-wrapper.tpl.php
+++ themes/agov/agov_whitlam/templates/comment-wrapper.tpl.php
@@ -11,15 +11,17 @@
 $comments = render($content['comments']);
 $comment_form = render($content['comment_form']);
 ?>
-<section class="comment__section <?php print $classes; ?>"<?php print $attributes; ?> id="comments">
+<section
+  class="comment__section <?php print $classes; ?>"<?php print $attributes; ?>
+  id="comments">
   <?php print render($title_prefix); ?>
   <?php if ($comments && $node->type != 'forum'): ?>
-    <?php if($total_comments_in_node == 1 ){ ?>
-    <h2 class="comment__section-title"><?php print t('!comments Comment',array('!comments'=>$total_comments_in_node)); ?></h2>
-    <?php  }else{ ?>
-    <h2 class="comment__section-title"><?php print t('!comments Comments',array('!comments'=>$total_comments_in_node)); ?></h2>
-    <?php } ?>
-  <?php endif; ?>  
+    <?php if ($total_comments_in_node == 1): ?>
+      <h2 class="comment__section-title"><?php print t('!comments Comment', array('!comments' => $total_comments_in_node)); ?></h2>
+    <?php else: ?>
+      <h2 class="comment__section-title"><?php print t('!comments Comments', array('!comments' => $total_comments_in_node)); ?></h2>
+    <?php endif; ?>
+  <?php endif; ?>
   <?php print render($title_suffix); ?>
 
   <?php print $comments; ?>
