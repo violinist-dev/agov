96deaeb2a0eec86de7dae90a7f7204a2b8c8ff77
bien.vu <tienbienit1992@gmail.com>
2015-06-03 14:10:42 +0700

    [#31042] Rename custom.js to script.js and add markup to superfish menu

---
+++ themes/agov/agov_whitlam/js/script.js
@@ -0,0 +1,46 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document) {
+
+  "use strict";
+
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.my_custom_behavior = {
+    attach: function(context, settings) {
+
+      // Place your code here.
+      // Menu responsive
+      $(".block-superfish").prepend('<div class="menu-icon open active">menu<i class="fa fa fa-bars"></i></div>');
+      $(".block-superfish").prepend('<div class="menu-icon close">close<i class="fa fa-close"></i></div>');
+      $("a.menuparent").after('<div class="sub-menu-icon"><i class="fa fa-plus"></i></div>');
+      $(".menu-icon.open").click(function(){
+        $(this).removeClass("active");
+        $(".menu-icon.close").addClass("active");
+        $(".sf-menu").addClass("active");
+      });
+
+      $(".menu-icon.close").click(function(){
+        $(this).removeClass("active");
+        $(".menu-icon.open").addClass("active");
+        $(".sf-menu").removeClass("active");
+      });
+
+      $(".sub-menu-icon").click(function(){
+        $(this).toggleClass("active");
+        $(this).next().toggleClass("active");
+      });
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
+
