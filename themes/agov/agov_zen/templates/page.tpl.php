5943f90e8012b302c152f0d27924fad2520e803c
JohnAlbin <virtually.johnalbin@gmail.com>
2015-06-17 15:51:05 +0800

    [#27820] Add accessible, but visually-hidden site name and slogan.

--- themes/agov/agov_zen/templates/page.tpl.php
+++ themes/agov/agov_zen/templates/page.tpl.php
@@ -35,6 +35,20 @@
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
     <?php endif; ?>
 
+    <?php if ($site_name || $site_slogan): ?>
+      <div class="header__name-and-slogan element-invisible">
+        <?php if ($site_name): ?>
+          <h1 class="header__site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
+
+        <?php if ($site_slogan): ?>
+          <div class="header__site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+
     <?php print render($page['header']); ?>
   </div>
 </header>
