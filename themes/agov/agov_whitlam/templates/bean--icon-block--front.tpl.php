2aea256e1df37b9eea20d8958f995d58435184a2
Magda Kostrzewa <mmkostrzewa@gmail.com>
2015-07-22 14:43:53 +1000

    [#32436] Added front icon bean teaser

---
+++ themes/agov/agov_whitlam/templates/bean--icon-block--front.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file
+ * Implementation for beans.
+ */
+?>
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <div class="box--icon box--with-border box--with-hover">
+    <?php if ($box_icon) : ?>
+      <div class="box--icon__left">
+        <i class="fa <?php print $box_icon; ?>"></i>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($box_link_to) || $box_text) : ?>
+      <div class="box--icon__right">
+        <?php if (!empty($box_link_to)) : ?>
+          <h3 class="box__title"><?php print render($box_link_to); ?></h3>
+        <?php endif; ?>
+
+        <?php if ($box_text) : ?>
+          <div class="box__content"><?php print render($box_text); ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
