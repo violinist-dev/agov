486a3b6760c363bcc0979819639371bdb5a291f8
Sam <sam@1485048.no-reply.drupal.org>
2015-09-24 16:42:52 +0800

    phpcs

--- agov/src/Tests/AgovTestBase.php
+++ agov/src/Tests/AgovTestBase.php
@@ -16,8 +16,10 @@ use Symfony\Component\BrowserKit\Request;
 abstract class AgovTestBase extends WebTestBase {
 
   /**
+   * Disable strict config checking.
+   *
    * Right now we can't enable strict config checking because of how page
-   * manager interacts with: https://www.drupal.org/node/2392057
+   * manager interacts with: https://www.drupal.org/node/2392057.
    *
    * @var bool
    */
