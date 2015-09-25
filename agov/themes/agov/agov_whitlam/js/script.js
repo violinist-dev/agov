36ce50426155d5aea569df9ea4bd6ff3849d2a95
Ben Dougherty <ben.dougherty@previousnext.com.au>
2015-09-25 10:05:38 +1000

    Fixed weird, non Drupal linting :/

--- agov/themes/agov/agov_whitlam/js/script.js
+++ agov/themes/agov/agov_whitlam/js/script.js
@@ -11,30 +11,29 @@
     attach: function (context, settings) {
 
       // Create an icon for toggling the menu.
-      var $navigation = $('.navigation'),
-          $open_icon = $('<div/>')
-              .addClass('menu-icon')
-              .addClass('open')
-              .addClass('active')
-              .html('menu<i class="fa fa fa-bars"></i>')
-              .prependTo($navigation);
-
-      // Clone the close icon and then update the text/icon.
+      var $navigation = $('.navigation');
+      var $open_icon = $('<div/>')
+            .addClass('menu-icon')
+            .addClass('open')
+            .addClass('active')
+            .html('menu<i class="fa fa fa-bars"></i>')
+            .prependTo($navigation);
       var $close_icon = $open_icon.clone();
+
       $close_icon
           .html('menu<i class="fa fa fa-close"></i>')
           .removeClass('active')
           .prependTo($navigation);
 
       // Find the open icon, hide it and show the close icon.
-      $open_icon.click(function() {
+      $open_icon.click(function () {
         $open_icon.removeClass('active');
         $close_icon.addClass('active');
         $navigation.find('ul').addClass('active');
       });
 
       // Find the close icon, hide it and show the open icon.
-      $close_icon.click(function() {
+      $close_icon.click(function () {
         $close_icon.removeClass('active');
         $open_icon.addClass('active');
         $navigation.find('ul').removeClass('active');
