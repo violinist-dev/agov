9be1895d7545fdc25a2f8064e8587a49bfe77a14
Sam <sam@1485048.no-reply.drupal.org>
2015-10-14 13:45:13 +0800

    [#35229] Toggle menu text.

--- agov/themes/agov/agov_whitlam/sass/components/primary-navigation/primary-navigation.js
+++ agov/themes/agov/agov_whitlam/sass/components/primary-navigation/primary-navigation.js
@@ -1,11 +1,22 @@
 (function ($) {
   Drupal.behaviors.agov_whitlam_primary_navigation = {
     attach: function (context, settings) {
-      $('.primary-navigation', context).once().each(function() {
-        var $menu = $(this);
-        $menu.find('.primary-navigation__title').click(function() {
-          $menu.toggleClass('primary-navigation--expanded');
-        })
+      $('.primary-navigation', context).once().each(function () {
+        var $menu = $(this),
+          $title = $menu.find('.primary-navigation__title');
+        var toggle = true;
+        $title.click(function () {
+          if (toggle) {
+            $menu.addClass('primary-navigation--expanded');
+            $title.text('Close');
+            toggle = false;
+          }
+          else {
+            $menu.removeClass('primary-navigation--expanded');
+            $title.text('Menu');
+            toggle = true;
+          }
+        });
       });
     }
   };
