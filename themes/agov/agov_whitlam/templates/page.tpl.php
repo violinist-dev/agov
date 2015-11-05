fb78aecc8eccd4fbf6eaa49a1c6461db286ce395
rikki_iki <rikki.bochow@gmail.com>
2015-11-06 09:50:21 +1100

    [#34073] add hidden homepage page title

--- themes/agov/agov_whitlam/templates/page.tpl.php
+++ themes/agov/agov_whitlam/templates/page.tpl.php
@@ -83,6 +83,8 @@
     <?php print render($title_prefix); ?>
     <?php if ($title): ?>
       <h1><?php print $title; ?></h1>
+    <?php elseif ($is_front): ?>
+      <h1 class="visually-hidden"><?php print t('Home'); ?></h1>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
     <?php print $messages; ?>
