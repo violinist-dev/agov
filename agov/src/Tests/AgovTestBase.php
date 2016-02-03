4b6c88ab820f95a95a5c90ab5a6b03bd04c22239
Ben Dougherty <ben.dougherty@previousnext.com.au>
2016-02-03 16:46:02 +0800

    Ignore the homepage for schema temp

--- agov/src/Tests/AgovTestBase.php
+++ agov/src/Tests/AgovTestBase.php
@@ -22,4 +22,16 @@ abstract class AgovTestBase extends WebTestBase {
    */
   protected $profile = 'agov';
 
+  protected static $configSchemaCheckerExclusions = array(
+    // Following are used to test lack of or partial schema. Where partial
+    // schema is provided, that is explicitly tested in specific tests.
+    'config_schema_test.noschema',
+    'config_schema_test.someschema',
+    'config_schema_test.schema_data_types',
+    'config_schema_test.no_schema_data_types',
+    // Used to test application of schema to filtering of configuration.
+    'config_test.dynamic.system',
+
+    'page_manager.page_variant.panels',
+  );
 }
