f90b05a3544c93b22aae182e0f721f6c448ae51d
Ben Dougherty <ben.dougherty@previousnext.com.au>
2016-02-05 13:37:29 +0800

    PHPCS

--- agov/src/EventSubscriber/AgovConfigDevelSubscriber.php
+++ agov/src/EventSubscriber/AgovConfigDevelSubscriber.php
@@ -1,24 +1,35 @@
 <?php
 
+/**
+ * @file
+ * Contains \Drupal\agov\EventSubscriber\AgovConfigDevelSubscriber.
+ */
+
 namespace Drupal\agov\EventSubscriber;
 
 use Drupal\config_devel\Event\ConfigDevelEvents;
 use Drupal\config_devel\Event\ConfigDevelSaveEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Config devel event subscriber.
+ */
 class AgovConfigDevelSubscriber implements EventSubscriberInterface {
 
   /**
-   * {@inheritdoc}
+   * The doSave callback.
+   *
+   * In aGov we provide optional modules and we don't want their config getting
+   * into our config objects during development, this handles that.
    */
   public function doSave(ConfigDevelSaveEvent $event) {
-    $optionalModules = ['workbench_moderation'];
+    $optional_modules = ['workbench_moderation'];
     $data = $event->getData();
 
     if (isset($data['third_party_settings'])) {
       foreach ($data['third_party_settings'] as $key => $value) {
         // If this is an optional module, then remove the config.
-        if (in_array($key, $optionalModules)) {
+        if (in_array($key, $optional_modules)) {
           unset($data['third_party_settings'][$key]);
 
           // Now find the module in the dependencies list and remove that.
