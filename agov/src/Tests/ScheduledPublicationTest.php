e8f06e0c2cf6e93485c56b11306079ee1b3e6eb1
Sam <sam@1485048.no-reply.drupal.org>
2016-02-12 13:29:29 +0800

    [#2662928] Fleshed out scheduled content test.

--- agov/src/Tests/ScheduledPublicationTest.php
+++ agov/src/Tests/ScheduledPublicationTest.php
@@ -19,7 +19,7 @@ class ScheduledPublicationTest extends AgovTestBase {
    *
    * @var array
    */
-  public static $modules = ['agov_scheduled_updates'];
+  public static $modules = ['agov_scheduled_updates', 'agov_workbench'];
 
   /**
    * The admin user.
@@ -34,16 +34,29 @@ class ScheduledPublicationTest extends AgovTestBase {
   public function setUp() {
     parent::setUp();
     $this->adminUser = $this->drupalCreateUser([
+      // Nodes.
       'administer nodes',
       'administer scheduled update types',
+      'create publication content',
+      'access content overview',
+      'administer content types',
+      'bypass node access',
+      // Scheduled updates.
       'view scheduled update entities',
       'create scheduled_publications scheduled updates',
       'delete any scheduled_publications scheduled updates',
       'edit any scheduled_publications scheduled updates',
-      'create publication content',
-      'access content overview',
-      'bypass node access',
-      'administer content types',
+      // Moderation state.
+      'administer moderation state transitions',
+      'administer moderation states',
+      'view any unpublished content',
+      'use published_draft transition',
+      'use draft_draft transition',
+      'use needs_review_needs_review transition',
+      'use published_published transition',
+      'use needs_review_published transition',
+      'use draft_published transition',
+      'use draft_needs_review transition',
     ]);
   }
 
@@ -52,17 +65,48 @@ class ScheduledPublicationTest extends AgovTestBase {
    */
   public function testScheduledPublication() {
     $this->drupalLogin($this->adminUser);
+
     // Create an unpublished node with a future publication date.
     $this->drupalGet('node/add/publication');
-    $this->assertText('scheduled publication');
+    $this->assertText('Scheduled publishing');
     $this->drupalPostForm(NULL, [], 'Add new scheduled publish date');
-    $this->drupalPostAjaxForm(NULL, [
-      'created[0][value][date]' => '2016-02-11',
-      'created[0][value][time]' => '20:57:29',
+    $this->drupalPostForm(NULL, [
+      'publish_date[form][inline_entity_form][update_timestamp][0][value][date]' => '2020-01-01',
+      'publish_date[form][inline_entity_form][update_timestamp][0][value][time]' => '00:00:00',
     ], 'Create scheduled publish date');
-    // @todo, rewrind the publication date to be in the past.
-    // @todo, run scheduled updates.
-    // @todo, assert the node is published.
+    $this->drupalPostForm(NULL, [
+      'title[0][value]' => $this->randomMachineName(),
+    ], 'Save and Create New Draft');
+
+    $node = $this->lastCreatedEntity('node');
+    $update = $this->lastCreatedEntity('scheduled_update');
+
+    // Ensure the node is unpublished.
+    $this->drupalLogout();
+    $this->drupalGet('/node/' . $node->id());
+
+    // Rewind the update time, so it's in the past.
+    $update->update_timestamp = REQUEST_TIME - 1500;
+    $update->save();
+
+    // Run all updates and revisit the node.
+    \Drupal::service('scheduled_updates.update_runner')->runAllUpdates();
+    $this->drupalGet('/node/' . $node->id());
+  }
+
+  /**
+   * Load the last created entity of a given type.
+   *
+   * @param string $type
+   *   The type of entity to load.
+   * @return \Drupal\Core\Entity\EntityInterface
+   *   A loaded entity.
+   */
+  protected function lastCreatedEntity($type) {
+    $id_key = \Drupal::entityTypeManager()->getDefinition($type)->getKey('id');
+    $results = \Drupal::entityQuery($type)->sort($id_key, 'DESC')->range(0, 1)->execute();
+    $id = array_shift($results);
+    return \Drupal::entityTypeManager()->getStorage($type)->load($id);
   }
 
 }
